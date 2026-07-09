2a2203f1439f42284bb0c86cb15e26f43186d92d
Roshanchoudhary <roshanchoudhary@hotmail.com>
2026-07-09 20:39:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import ScriptConverter from './src/converter.js';
+import { ScriptConverter } from './src/converter.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
